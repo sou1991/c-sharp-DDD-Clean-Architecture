198f105abb28f9794817f5d1ef4d33944ee874fe
sou_devlop <sou.business0109@gmail.com>
2020-03-29 20:31:44 +0900

    #1 Project-Build プロジェクト構築

---
+++ StackDreamPig/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace StackDreamPig.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
