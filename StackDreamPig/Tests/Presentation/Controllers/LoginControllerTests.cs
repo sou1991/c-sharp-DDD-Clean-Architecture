946f240cc2149a474219d8c47df9e5a09dde5432
sou_devlop <sou.business0109@gmail.com>
2020-04-01 21:11:07 +0900

    #4 TOPページ・テストプロジェクトの作成　Create Top Page And Tests Project

---
+++ StackDreamPig/Tests/Presentation/Controllers/LoginControllerTests.cs
@@ -0,0 +1,29 @@
+﻿using AutoMoq;
+using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+using Presentation.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tests.Presentation.Controllers
+{
+    class LoginControllerTests
+    {
+        private LoginController _LoginController;
+        private AutoMoqer _mocker;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mocker = new AutoMoqer();
+            _LoginController = new LoginController();
+        }
+        [Test]
+        public void TestGetViewType()
+        {
+            var result = _LoginController.Login();
+            Assert.That(result, Is.TypeOf<ViewResult>());
+        }
+    }
+}
