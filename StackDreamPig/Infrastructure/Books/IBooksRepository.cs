0e25dfcdf2cbf9bd98a64022cf9e9a60b3db1c92
sou_devlop <sou.business0109@gmail.com>
2021-03-07 13:00:44 +0900

    Add Books Repository and interface

---
+++ StackDreamPig/Infrastructure/Books/IBooksRepository.cs
@@ -0,0 +1,17 @@
+﻿using Entities;
+using System;
+using System.Linq;
+
+namespace Infrastructure.Books
+{
+    public interface IBooksRepositorySS
+    {
+        BooksEntity Find(string targetID, DateTime targetDate, bool isRegist = false);
+
+        IQueryable<BooksEntity> FindSingle();
+
+        void Create(BooksEntity memberEntity);
+
+        void Save();
+    }
+}
